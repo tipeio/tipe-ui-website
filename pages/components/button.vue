366d67b4cf3e7b42478c8acc9f1b53f4aee0e5a1
Olivia Oddo <olivia.oddo@gmail.com>
2018-08-21 15:38:58 -0700

    feat(button): add button examples and code snippets

--- pages/components/button.vue
+++ pages/components/button.vue
@@ -1,12 +1,38 @@
 <template>
   <component-grid>
     <component-title title="Button" />
-    <tipe-card>
-      <tipe-button>Button</tipe-button>
+    <attr-title title="Sizes"/>
+    <tipe-card class="flex">
+      <tipe-button size="mini">Mini</tipe-button>
+      <tipe-button size="small">Small</tipe-button>
+      <tipe-button size="medium">Medium</tipe-button>
+      <tipe-button size="large">Large</tipe-button>
     </tipe-card>
-    <component-code-block sourcecode="console.log('hello')"/>
-    <h2 class="sub-title">Attributes</h2>
-    <ui-table :attributes="attributes" />
+    <component-code-block :sourcecode="sizeSourcecode"/>
+    <attr-title title="Colors"/>
+    <tipe-card class="flex">
+      <tipe-button color="primary">Primary</tipe-button>
+      <tipe-button color="success">Success</tipe-button>
+      <tipe-button color="info">Info</tipe-button>
+      <tipe-button color="warning">Warning</tipe-button>
+      <tipe-button color="danger">Danger</tipe-button>
+    </tipe-card>
+    <component-code-block :sourcecode="colorSourcecode"/>
+    <attr-title title="Outline"/>
+    <tipe-card class="flex">
+      <tipe-button outline>Outline</tipe-button>
+      <tipe-button outline color="success">Outline</tipe-button>
+      <tipe-button outline color="info">Outline</tipe-button>
+      <tipe-button outline color="warning">Outline</tipe-button>
+      <tipe-button outline color="danger">Outline</tipe-button>
+    </tipe-card>
+    <component-code-block :sourcecode="outlineSourcecode"/>
+    <attr-title title="Status"/>
+    <tipe-card class="flex">
+      <tipe-button disabled>Disabled</tipe-button>
+      <tipe-button waiting>Waiting</tipe-button>
+    </tipe-card>
+    <component-code-block :sourcecode="statusSourcecode"/>
   </component-grid>
 </template>
 
@@ -16,7 +42,7 @@ import UiTable from '~/components/Table.vue'
 import ComponentTitle from '~/components/Component/Title.vue'
 import ComponentGrid from '~/components/Component/Grid.vue'
 import ComponentCodeBlock from '~/components/Component/CodeBlock.vue'
-
+import AttrTitle from '~/components/Component/AttrTitle.vue'
 export default {
   layout: 'docs',
   components: {
@@ -25,7 +51,8 @@ export default {
     UiTable,
     ComponentTitle,
     ComponentGrid,
-    ComponentCodeBlock
+    ComponentCodeBlock,
+    AttrTitle
   },
   data() {
     return {
@@ -44,15 +71,44 @@ export default {
           values: 'purple danger',
           default: 'purple'
         }
-      ]
+      ],
+      sizeSourcecode: `<tipe-card>
+  <tipe-button size="mini">Mini</tipe-button>
+  <tipe-button size="small">Small</tipe-button>
+  <tipe-button size="medium">Medium</tipe-button>
+  <tipe-button size="large">Large</tipe-button>
+</tipe-card>`,
+      colorSourcecode: `<tipe-card>
+  <tipe-button color="primary">Mini</tipe-button>
+  <tipe-button color="success">Small</tipe-button>
+  <tipe-button color="info">Medium</tipe-button>
+  <tipe-button color="warning">Large</tipe-button>
+  <tipe-button color="danger">Large</tipe-button>
+</tipe-card>`,
+      statusSourcecode: `<tipe-card>
+  <tipe-button disabled>Disabled</tipe-button>
+  <tipe-button waiting>Waiting</tipe-button>
+</tipe-card>`,
+      outlineSourcecode: `<tipe-card>
+  <tipe-button outline>Outline</tipe-button>
+  <tipe-button outline color="success">Outline</tipe-button>
+  <tipe-button outline color="danger">Outline</tipe-button>
+  <tipe-button outline color="warning">Outline</tipe-button>
+</tipe-card>`
     }
   }
 }
 </script>
 
 <style lang="postcss" scoped>
+.flex {
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  padding: 2rem 0;
+}
 .sub-title {
-  color: var(--text-purple);
+  color: var(--text-purple-gray);
   font-size: 2rem;
 }
 </style>

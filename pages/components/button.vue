1fa9565f029b0a44d5f8f0879a20fd13c7f34829
Olivia Oddo <olivia.oddo@gmail.com>
2018-08-21 14:20:20 -0700

    Highlightjs (#1)
    * feat(highlight): add highlight js
    
    * polish(highlight): add highlight-js css theme
    
    * feat(highlight): add code block component and add ui colors to highlight.css
    
    * polish(colors): add color to global colors

--- pages/components/button.vue
+++ pages/components/button.vue
@@ -1,7 +1,10 @@
 <template>
   <component-grid>
     <component-title title="Button" />
-    <tipe-card><tipe-button>Button</tipe-button></tipe-card>
+    <tipe-card>
+      <tipe-button>Button</tipe-button>
+    </tipe-card>
+    <component-code-block sourcecode="console.log('hello')"/>
     <h2 class="sub-title">Attributes</h2>
     <ui-table :attributes="attributes" />
   </component-grid>
@@ -12,6 +15,7 @@ import { TipeCard, TipeButton } from '@tipe/ui'
 import UiTable from '~/components/Table.vue'
 import ComponentTitle from '~/components/Component/Title.vue'
 import ComponentGrid from '~/components/Component/Grid.vue'
+import ComponentCodeBlock from '~/components/Component/CodeBlock.vue'
 
 export default {
   layout: 'docs',
@@ -20,7 +24,8 @@ export default {
     TipeButton,
     UiTable,
     ComponentTitle,
-    ComponentGrid
+    ComponentGrid,
+    ComponentCodeBlock
   },
   data() {
     return {

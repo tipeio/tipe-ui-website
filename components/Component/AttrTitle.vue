88790684101545cc7c6c8f976fea7acdb97e2085
Olivia Oddo <olivia.oddo@gmail.com>
2018-08-21 15:30:34 -0700

    feat(button): add button examples and code snippets

---
+++ components/Component/AttrTitle.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2 :data-tipe-ui="$options.name">{{title}}</h2>
+</template>
+
+<script>
+import vueTypes from 'vue-types'
+
+export default {
+  name: 'AttrTitle',
+  props: {
+    title: vueTypes.string.def('')
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+[data-tipe-ui='AttrTitle'] {
+  color: var(--text-purple-gray);
+  font-size: 2rem;
+}
+</style>

ea4316ade177658ce9977edf200e065a313cc590
Olivia Oddo <olivia.oddo@gmail.com>
2018-08-27 18:41:15 -0700

    feat(callout-card): add callout card

---
+++ components/Component/CalloutCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalloutCard'
+}
+</script>
+
+<style lang="postcss" scoped>
+.card {
+  display: flex;
+  background-color: #f3f8f3;
+  border: 1px solid #50af51;
+  border-radius: 3px;
+  border-left: 10px solid #1cc5bc;
+  width: 100%;
+  padding: 1.625rem;
+}
+</style>

8cc88ab0a612859d8b839d66bc4c3be9608b0c3d
Olivia Oddo <olivia.oddo@gmail.com>
2018-08-28 17:06:22 -0700

    feat(home-section-2): add second section of the homepage

--- pages/index.vue
+++ pages/index.vue
@@ -13,21 +13,45 @@
       </div>
       <space-people class="space"/>
     </section>
+    <section class="info">
+      <purple-shape class="purple-shape"/>
+      <div class="info-text">
+        <h3 class="info-title">Projects that use Tipe Ui</h3>
+        <logo-with-text />
+      </div>
+      <div class="card-row">
+        <project-card title="Guide" desc="Understand the design guidelines, helping designers 
+build product that's logically sound, reasonably 
+structured and easy to use." svg="guide"/>
+        <project-card title="Component" desc="Understand the design guidelines, helping designers 
+build product that's logically sound, reasonably 
+structured and easy to use." svg="component"/>
+        <project-card title="Resources" desc="Understand the design guidelines, helping designers 
+build product that's logically sound, reasonably 
+structured and easy to use." svg="resources"/>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import UiNav from '~/components/Nav'
 import UiButton from '~/components/Component/Button.vue'
+import ProjectCard from '~/components/Component/ProjectCard.vue'
 import SpacePeople from '~/assets/space-people.svg'
 import Stars from '~/assets/stars.svg'
+import LogoWithText from '~/assets/logo-with-text.svg'
+import PurpleShape from '~/assets/purple-shape.svg'
 
 export default {
   components: {
     UiNav,
     SpacePeople,
+    ProjectCard,
     UiButton,
-    Stars
+    Stars,
+    LogoWithText,
+    PurpleShape
   }
 }
 </script>
@@ -36,7 +60,8 @@ export default {
 .outter-grid {
   display: grid;
   grid-template-columns: 1fr 7fr 1fr;
-  grid-template-areas: '. header .';
+  grid-template-areas: '. header .' '. info .';
+  grid-template-rows: auto auto;
   grid-column-gap: 1rem;
 }
 
@@ -95,6 +120,48 @@ export default {
 
 .space {
   grid-area: space;
+  grid-row-start: 4;
+}
+
+.info {
+  grid-area: info;
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  grid-template-rows: auto 60px auto 10px;
+  grid-template-areas: 'info-text' '.' 'card-row' 'purple';
+}
+
+.card-row {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 2;
+  grid-area: card-row;
+}
+
+.info-title {
+  color: var(--text-purple);
+  font-family: Lato;
+  font-size: 40px;
+  font-weight: bold;
+  letter-spacing: 0.6px;
+  line-height: 48px;
+  text-align: center;
+  margin: 25px 0;
+}
+
+.info-text {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  z-index: 2;
+  grid-area: info-text;
+}
+
+.purple-shape {
+  z-index: 1;
+  grid-area: purple;
   grid-row-start: 4;
 }
 </style>

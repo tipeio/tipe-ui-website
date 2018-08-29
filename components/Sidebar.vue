16864b73662061f61c8d56e6fd684ebcd345b08b
Olivia Oddo <olivia.oddo@gmail.com>
2018-08-29 16:59:29 -0700

    feat(sponsor): add sponsor in the sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,14 +1,21 @@
 <template>
-  <div :data-tipe-ui="$options.name" >
+  <div>
     <tipe-scroll class="sidebar">
       <List>
-        <Heading>Getting Started</Heading>
+        <Heading>Sponsors</Heading>
+        <sponsor logo="sample-logo" text="It was a humorously perilous business 
+        for both of us. For, before we proceed 
+        further," />
+        <nuxt-link :class="{active: $nuxt.$route.name.includes('changelog')}" to="/changelog">
+          <Heading>Changelog</Heading>
+        </nuxt-link>
+        <Heading>Development</Heading>
 
         <nuxt-link to="/component/install">
-          <Item>Install</Item>
+          <Item>Installation</Item>
         </nuxt-link>
 
-        <nuxt-link to="/component/quick-start">
+        <nuxt-link :active="{active: $nuxt.$route.name.includes('quick-start')}" to="/component/quick-start">
           <Item>Quick Start</Item>
         </nuxt-link>
 
@@ -61,7 +68,104 @@
         <nuxt-link to="/component/search">
           <Item>Search</Item>
         </nuxt-link>
+      <Heading>Components</Heading>
 
+        <SubHeading>Buttons</SubHeading>
+
+        <nuxt-link to="/component/button">
+          <Item>Button</Item>
+        </nuxt-link>
+
+        <nuxt-link to="/component/icon-button">
+          <Item>Icon Button</Item>
+        </nuxt-link>
+
+        <SubHeading>Form</SubHeading>
+
+        <nuxt-link to="/component/text-input">
+          <Item>Text Input</Item>
+        </nuxt-link>
+
+        <nuxt-link to="/component/number-input">
+          <Item>Number Input</Item>
+        </nuxt-link>
+
+        <nuxt-link to="/component/textarea">
+          <Item>Textarea</Item>
+        </nuxt-link>
+
+        <nuxt-link to="/component/radio">
+          <Item>Radio</Item>
+        </nuxt-link>
+
+        <nuxt-link to="/component/checkbox">
+          <Item>Checkbox</Item>
+        </nuxt-link>
+
+        <nuxt-link to="/component/switch">
+          <Item>Switch</Item>
+        </nuxt-link>
+
+        <nuxt-link to="/component/select">
+          <Item>Select</Item>
+        </nuxt-link>
+
+        <nuxt-link to="/component/select-button">
+          <Item>Select Button</Item>
+        </nuxt-link>
+
+        <nuxt-link to="/component/search">
+          <Item>Search</Item>
+        </nuxt-link>
+        <Heading>Components</Heading>
+
+        <SubHeading>Buttons</SubHeading>
+
+        <nuxt-link to="/component/button">
+          <Item>Button</Item>
+        </nuxt-link>
+
+        <nuxt-link to="/component/icon-button">
+          <Item>Icon Button</Item>
+        </nuxt-link>
+
+        <SubHeading>Form</SubHeading>
+
+        <nuxt-link to="/component/text-input">
+          <Item>Text Input</Item>
+        </nuxt-link>
+
+        <nuxt-link to="/component/number-input">
+          <Item>Number Input</Item>
+        </nuxt-link>
+
+        <nuxt-link to="/component/textarea">
+          <Item>Textarea</Item>
+        </nuxt-link>
+
+        <nuxt-link to="/component/radio">
+          <Item>Radio</Item>
+        </nuxt-link>
+
+        <nuxt-link to="/component/checkbox">
+          <Item>Checkbox</Item>
+        </nuxt-link>
+
+        <nuxt-link to="/component/switch">
+          <Item>Switch</Item>
+        </nuxt-link>
+
+        <nuxt-link to="/component/select">
+          <Item>Select</Item>
+        </nuxt-link>
+
+        <nuxt-link to="/component/select-button">
+          <Item>Select Button</Item>
+        </nuxt-link>
+
+        <nuxt-link to="/component/search">
+          <Item>Search</Item>
+        </nuxt-link>
       </List>
     </tipe-scroll>
   </div>
@@ -74,6 +178,7 @@ import List from './List/List.vue'
 import Heading from './List/Heading.vue'
 import SubHeading from './List/SubHeading.vue'
 import Item from './List/Item.vue'
+import Sponsor from '~/components/Component/Sponsor.vue'
 
 export default {
   name: 'UiSidebar',
@@ -82,19 +187,21 @@ export default {
     List,
     Heading,
     SubHeading,
-    Item
+    Item,
+    Sponsor
   }
 }
 </script>
 
 <style lang="postcss" scoped>
-[data-tipe-ui='UiSidebar'] {
-}
-
 .sidebar {
   height: 100%;
   padding-left: 3rem;
   padding-top: 3rem;
   border-right: 1px solid rgba(56, 74, 101, 0.5);
+}
+
+.nuxt-link-active > div {
+  color: var(--teal);
 }
 </style>

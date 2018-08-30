6431e092f1da98b52f26cae125e9d6329ff1d252
Olivia Oddo <olivia.oddo@gmail.com>
2018-08-30 15:00:51 -0700

    polish(footer): add content to footer ansd create layout

--- components/Home/Footer.vue
+++ components/Home/Footer.vue
@@ -13,6 +13,18 @@
         <nav-dropdown footer :links="languages" text="English"/>      
         <nav-link footer to="/support" text="Support"/>
       </div>
+      <div class="line-break"><line-break/></div>
+      <div class="logo">
+        <tipe-logo />
+        <p>Join Us On</p>
+      </div>
+      <div class="social">
+        <div class="social-link"><google /></div>
+        <div class="social-link"><google /></div>
+        <div class="social-link"><google /></div>
+        <div class="social-link"><google /></div>
+      </div>
+      <p class="copyright">©2018 Tipe. All Rights reserved.</p>
     </div>
   </footer>
 </template>
@@ -20,12 +32,16 @@
 <script>
 import NavLink from '~/components/Nav/NavLink.vue'
 import NavDropdown from '~/components/Nav/NavDropdown.vue'
+import TipeLogo from '~/assets/tipe-logo.svg'
+import Google from '~/assets/google.svg'
 
 export default {
   name: 'Footer',
   components: {
     NavLink,
-    NavDropdown
+    NavDropdown,
+    TipeLogo,
+    Google
   },
   data() {
     return {
@@ -52,8 +68,12 @@ export default {
   height: 400px;
   grid-row-end: 6;
   display: grid;
-  grid-template-rows: 1fr 1fr;
+  grid-template-rows: 1fr auto 1fr;
   grid-template-columns: 175px auto 300px;
+  grid-template-areas:
+    '. overlay overlay'
+    'content content content'
+    'overlay-2 overlay-2 .';
 }
 
 .overlay {
@@ -66,6 +86,7 @@ export default {
   z-index: 3;
   grid-column-start: 2;
   grid-row-start: 1;
+  grid-area: overlay;
 }
 
 .overlay-2 {
@@ -81,20 +102,86 @@ export default {
   grid-column-start: 1;
   grid-column-end: 3;
   width: 100%;
+  grid-area: overlay-2;
 }
 
 .content {
   grid-row-start: 1;
   grid-column-start: 2;
-  width: 100%;
   z-index: 5;
   align-items: center;
   justify-content: center;
+  grid-area: content;
+  grid-row-start: 1 !important;
+  grid-row-end: 4 !important;
+  display: grid;
+  grid-template-rows: 1fr 3px 1fr 1fr;
+  grid-template-areas: 'links' 'line-break' 'logo' 'social';
 }
 
 .links {
   grid-area: right;
   display: flex;
   justify-content: space-evenly;
+  grid-area: links;
+}
+
+.logo {
+  grid-area: logo;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.logo > p {
+  opacity: 0.5;
+  color: #ffffff;
+  font-family: Lato;
+  font-size: 13px;
+  letter-spacing: 0.46px;
+  line-height: 28px;
+  text-align: center;
+  text-transform: uppercase;
+  margin: 28px;
+}
+
+.social {
+  grid-area: social;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-self: start;
+}
+
+.social-link {
+  height: 40px;
+  width: 40px;
+  background-color: #fff;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+}
+
+.copyright {
+  opacity: 0.5;
+  color: #ffffff;
+  font-family: Lato;
+  font-size: 13px;
+  letter-spacing: 0.46px;
+  line-height: 28px;
+  text-align: center;
+}
+
+.line-break {
+  grid-area: line-break;
+  border: 0.5px solid #fff;
+  border-radius: 50%;
+  height: 3px;
+  opacity: 0.2;
+  justify-self: center;
+  width: 60%;
+  margin-bottom: 2rem;
 }
 </style>

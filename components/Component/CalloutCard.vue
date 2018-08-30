8ffa64f24b7c152be380edffed2a91a80f8701d9
Olivia Oddo <olivia.oddo@gmail.com>
2018-08-29 17:23:04 -0700

    polish(card-colors): add different types for callout cards

--- components/Component/CalloutCard.vue
+++ components/Component/CalloutCard.vue
@@ -1,23 +1,46 @@
 <template>
-  <div class="card">
+  <div class="card" :class="{[color]: true}">
     <slot/>
   </div>
 </template>
 
 <script>
+import vueTypes from 'vue-types'
+
 export default {
-  name: 'CalloutCard'
+  name: 'CalloutCard',
+  props: {
+    color: vueTypes.string.def('teal')
+  }
 }
 </script>
 
 <style lang="postcss" scoped>
 .card {
   display: flex;
-  background-color: rgba(104, 73, 252, 0.05);
-  border: 1px solid var(--teal);
+  background-color: #f7f8fb;
   border-radius: 3px;
-  border-left: 10px solid var(--teal);
   width: 100%;
   padding: 1.625rem;
+}
+
+.teal {
+  border: 1px solid var(--teal);
+  border-left: 10px solid var(--teal);
+}
+
+.purple {
+  border: 1px solid var(--purple);
+  border-left: 10px solid var(--purple);
+}
+
+.orange {
+  border: 1px solid var(--orange);
+  border-left: 10px solid var(--orange);
+}
+
+.red {
+  border: 1px solid var(--red);
+  border-left: 10px solid var(--red);
 }
 </style>
